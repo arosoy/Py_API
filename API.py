2cd390f9798bf7bc8b373ef14d6392a900ffb617
arosoy <suvaol@yandex.ru>
2019-04-28 19:37:08 +0400

    перед коментом

--- API.py
+++ API.py
@@ -2,30 +2,13 @@ import pygame
 import requests
 import sys
 import os
-response = None
-pygame.init()
-screen = pygame.display.set_mode((1000, 650))
-a = 135.626082
-b = -26.923984
-while True:
-    for event in pygame.event.get():
-        # при закрытии окна
-        if event.type == pygame.KEYDOWN:
-            if event.type == pygame.QUIT:
-                running = False
-            if event.key == pygame.K_LEFT:
-                a -= 1
-            elif event.key == pygame.K_RIGHT:
-                a += 1
-            elif event.key == pygame.K_UP:
-                b += 1
-            elif event.key == pygame.K_DOWN:
-                b -= 1
 
+def Load_map(x, y):
+    response = None
+    b = pygame.Surface((276, 276))
     try:
-        map_request = "http://static-maps.yandex.ru/1.x/?ll="+str(a)+','+str(b)+"&spn=30.5,23.5&l=map"
+        map_request = "http://static-maps.yandex.ru/1.x/?&bbox="+str(x)+","+str(y)+"~"+str(x+3)+","+str(y+3)+"&l=sat"
         response = requests.get(map_request)
-
         if not response:
             print("Ошибка выполнения запроса:")
             print("Http статус:", response.status_code, "(", response.reason, ")")
@@ -38,17 +21,59 @@ while True:
     try:
         with open(map_file, "wb") as file:
             file.write(response.content)
+
     except IOError as ex:
         print("Ошибка записи временного файла:", ex)
         sys.exit(2)
-
-
-
-    screen.blit(pygame.image.load(map_file), (0, 0))
-    pygame.display.flip()
-    while pygame.event.wait().type != pygame.QUIT:
-        pass
-    pygame.quit()
-
-
+    b.blit(pygame.image.load(map_file), (0, 0))
     os.remove(map_file)
+    return b
+
+response = None
+try:
+    # map_request = "http://static-maps.yandex.ru/1.x/?ll=111.5,-32&spn=3,3&l=sat&size=650,450"
+    map_request = "http://static-maps.yandex.ru/1.x/?&bbox=115,-32~118,-35&l=sat"
+    map_request1 = "http://static-maps.yandex.ru/1.x/?&bbox=118,-32~121,-35&l=sat"
+    response = requests.get(map_request)
+    response1 = requests.get(map_request1)
+
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+except:
+    print("Запрос не удалось выполнить. Проверьте наличие сети Интернет.")
+    sys.exit(1)
+
+map_file = "map.png"
+map_file1 = "map1.png"
+try:
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+    with open(map_file1, "wb") as file:
+        file.write(response1.content)
+
+except IOError as ex:
+    print("Ошибка записи временного файла:", ex)
+    sys.exit(2)
+
+
+pygame.init()
+screen = pygame.display.set_mode((900, 650))
+screen.blit(pygame.image.load(map_file), (0, 0))
+screen.blit(pygame.image.load(map_file1), (276, 0))
+#for i in range(650):
+    #for j in range(450):
+        #r, g, b, a = background.get_at((i, j))
+        #if r*2 < b or g*2 < b:
+            #pygame.draw.line(background, (0, 0, 0), [i, j], [i, j])
+#screen.blit(background, (0, 0))
+# pygame.draw.rect(screen, (255, 71, 128), (290, 190, 20, 20), 1)
+# background.blit(pix_map[n], (j * 96, i * 96))
+pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+pygame.quit()
+
+
+# os.remove(map_file)
